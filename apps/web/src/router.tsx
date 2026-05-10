e0a347cdeec5d7fe36ef6a6c4903a28fbd65901f
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-11 02:18:19 +0800

    chore: initialize users feature

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -4,15 +4,17 @@ import { RootLayout } from './layouts/RootLayout';
 import { AuthLayout } from './layouts/AuthLayout';
 
 import { HomePage } from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
 import { SignInPage } from './features/auth/pages/SignInPage';
 import { SignUpPage } from './features/auth/pages/SignUpPage';
-import NotFoundPage from './pages/NotFoundPage';
+import { UserProfilePage } from './features/users/pages/UserProfilePage';
 
 export function AppRouter() {
   return (
     <Routes>
       <Route element={<RootLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/profile/:id" element={<UserProfilePage />} />
       </Route>
       <Route element={<AuthLayout />}>
         <Route path="/sign-in" element={<SignInPage />} />
