6027cb37e12c8d54e2172211919c99136d4e8e91
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-05-10 14:25:29 +0800

    feat: integrate auth in api

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { env } from './config/env.js';
+import { fromNodeHeaders, toNodeHandler } from 'better-auth/node';
+import { auth } from './auth/auth.js';
 
 import usersRoutes from './modules/users/users.routes.js';
 
@@ -13,10 +15,19 @@ app.use(
   }),
 );
 
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello' });
+});
+
+app.get('/api/me', async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  return res.json(session);
 });
 
 app.use('/users', usersRoutes);
